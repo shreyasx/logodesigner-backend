91cfbb6d5a1599eefc49dafe7006a1d6efacaabd
Shreyas <shreyxs@gmail.com>
2021-05-20 03:27:22 +0530

    updated some code

--- public/index.js
+++ public/index.js
@@ -1,37 +1,42 @@
-const CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/shrey/upload';
-const CLOUDINARY_UPLOAD_PRESEST = 'lom4m0su';
+const CLOUDINARY_URL = "https://api.cloudinary.com/v1_1/shrey/upload";
+const CLOUDINARY_UPLOAD_PRESEST = "lom4m0su";
 
 window.onload = function () {
-  var form = document.getElementById('form');
-  var fileUpload = document.getElementById('file-upload');
-  var formData = new FormData();
-  fileUpload.addEventListener('change', e => {
-    var file = e.target.files[0];
-    formData.append('file', file);
-    formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESEST);
-  });
+	var form = document.getElementById("form");
+	var fileUpload = document.getElementById("file-upload");
+	var formData = new FormData();
+	fileUpload.addEventListener("change", e => {
+		var file = e.target.files[0];
+		formData.append("file", file);
+		formData.append("upload_preset", CLOUDINARY_UPLOAD_PRESEST);
+	});
 
-  form.addEventListener('submit', e => {
-    e.preventDefault();
-    axios({
-      url: CLOUDINARY_URL,
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      data: formData
-    }).then(res => {
-      const data = {
-        name: document.getElementById('name').value.toLowerCase(),
-        desc: document.getElementById('desc').value,
-        category: document.getElementById('categ').value.toLowerCase(),
-        hashs: document.getElementById('hash').value.toLowerCase(),
-        url: res.data.secure_url,
-        isSignedin: true
-      };
-      fetch('/upload', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data)
-      }).then(console.log)
-    }).catch(console.log);
-  });
-}
+	form.addEventListener("submit", e => {
+		e.preventDefault();
+		axios({
+			url: CLOUDINARY_URL,
+			method: "POST",
+			headers: { "Content-Type": "application/x-www-form-urlencoded" },
+			data: formData,
+		})
+			.then(res => {
+				const data = {
+					name: document.getElementById("name").value.toLowerCase(),
+					desc: document.getElementById("desc").value,
+					category: document.getElementById("categ").value.toLowerCase(),
+					hashs: document.getElementById("hash").value.toLowerCase(),
+					url: res.data.secure_url,
+					isSignedin: true,
+				};
+				fetch("/upload", {
+					method: "POST",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify(data),
+				})
+					.then(resp => resp.json())
+					.then(console.log)
+					.catch(console.log);
+			})
+			.catch(console.log);
+	});
+};
